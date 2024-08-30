8082b5ed615bd6432dd983143e6b1fdc87d59ffb
frankvalli <francescovalli99@gmail.com>
2024-08-30 22:18:57 +0200

    Fixed command pushing and argument counting #9

--- command.c
+++ command.c
@@ -11,30 +11,46 @@ command_t *init_command() {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);        
     }
+    cmd -> args[0] = NULL;
     cmd -> dest = (char **) malloc(sizeof(char *));
     if (cmd -> dest == NULL) {
         write(STDERR_FILENO, error_message, strlen(error_message));
         exit(1);        
     }
+    cmd -> dest[0] = NULL;
     return cmd;
 }
 
 void push_arg(command_t *cmd, char *arg) {
-    cmd -> args = (char **) realloc(cmd -> args, (num_args(cmd) + 1) * sizeof(char *));
-    cmd -> args[num_args(cmd) - 1] = strdup(arg);
-    cmd -> args[num_args(cmd)] = NULL;
+    int n = num_args(cmd);
+    cmd -> args = (char **) realloc(cmd -> args, (n + 2) * sizeof(char *));
+    cmd -> args[n + 1] = NULL;
+    cmd -> args[n] = strdup(arg);
     return;
 }
 
 void push_dest(command_t *cmd, char *dest) {
-    cmd -> dest = (char **) realloc(cmd -> dest, (num_dests(cmd) + 1) * sizeof(char *));
-    cmd -> dest[num_dests(cmd) - 1] = strdup(dest);
-    cmd -> dest[num_dests(cmd)] = NULL;
+    int n = num_dests(cmd);
+    cmd -> dest = (char **) realloc(cmd -> dest, (n + 2) * sizeof(char *));
+    cmd -> dest[n + 1] = NULL;
+    cmd -> dest[n] = strdup(dest);
 }
 
 void destroy_command(command_t **cmd) {
-    for (int i = 0; i < num_args(*cmd); i++) free((*cmd) -> args[i]);
-    for (int i = 0; i < num_dests(*cmd); i++) free((*cmd) -> dest[i]);
+    if (*cmd == NULL) {
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
+    }
+    int n_args = num_args(*cmd);
+    int n_dests = num_dests(*cmd);
+    for (int i = 0; i < n_args; i++) {
+        free((*cmd) -> args[i]);
+        (*cmd) -> args[i] = NULL;
+    }
+    for (int i = 0; i < n_dests; i++) {
+        free((*cmd) -> dest[i]);
+        (*cmd) -> dest[i] = NULL;
+    }
     free((*cmd) -> args);
     (*cmd) -> args = NULL;
     free((*cmd) -> dest);
@@ -44,6 +60,7 @@ void destroy_command(command_t **cmd) {
 }
 
 int num_args(command_t *cmd) {
+    if (cmd == NULL) return -1;
     char *pargs = cmd -> args[0];
     int n = 0;
     while(pargs != NULL) {
@@ -54,6 +71,7 @@ int num_args(command_t *cmd) {
 }
 
 int num_dests(command_t *cmd) {
+    if (cmd == NULL) return -1;
     char *pdest = cmd -> dest[0];
     int n = 0;
     while(pdest != NULL) {
